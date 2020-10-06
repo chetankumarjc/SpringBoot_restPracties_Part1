74c136588d5bde6236efbac631cb53c1ef540dd0
DESKTOP-BA6SGJJ\Admin <chetankumarnmit@gmail.com>
2020-10-06 17:09:25 +0530

    Spring boot initial commit on Oct 16th

---
+++ SpringBoot_restPracties_Part1/src/main/java/com/restservice/org/SpringBootRestPractiesPart1Application.java
@@ -0,0 +1,13 @@
+package com.restservice.org;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestPractiesPart1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestPractiesPart1Application.class, args);
+	}
+
+}
