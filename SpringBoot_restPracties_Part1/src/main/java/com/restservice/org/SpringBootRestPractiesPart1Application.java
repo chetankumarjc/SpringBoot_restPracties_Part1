ce4e45492c81cb745f8be4bf910ed24dcb07c0de
DESKTOP-BA6SGJJ\Admin <chetankumarnmit@gmail.com>
2020-10-06 17:16:23 +0530

    Test commit

--- SpringBoot_restPracties_Part1/src/main/java/com/restservice/org/SpringBootRestPractiesPart1Application.java
+++ SpringBoot_restPracties_Part1/src/main/java/com/restservice/org/SpringBootRestPractiesPart1Application.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootRestPractiesPart1Application {
 
 	public static void main(String[] args) {
+		System.out.println(" Main Start");
 		SpringApplication.run(SpringBootRestPractiesPart1Application.class, args);
+		System.out.println(" Main End");
 	}
 
 }
